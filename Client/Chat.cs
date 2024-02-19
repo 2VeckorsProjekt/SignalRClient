61193ae5227e60d3b631c554232623bc88ad8f2c
bjornjohanf <bjornjohanf@gmail.com>
2024-02-19 11:19:39 +0100

    Client

---
+++ Client/Chat.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Client;
+
+internal class Chat
+{
+    public HubConnection connection { get; private set; }
+    string ip;
+    string port;
+    string endpoint;
+    TimeOnly time = new TimeOnly();
+    string url => $"wss://{ip}:{port}/{endpoint}";
+    
+    public Chat(string ip, string port, string endpoint)
+    {
+        this.ip = ip;
+        this.port = port;
+        this.endpoint = endpoint;
+
+        this.connection = new HubConnectionBuilder().WithUrl(url).Build();
+        connection.StartAsync().Wait();
+
+        connection.On<string>("ReceiveMessage", (message) =>
+        {
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.WriteLine($"{time.ToString()}: {message}");
+            Console.Write("Enter message: ");
+        });
+
+        Sender();
+    }
+
+    void Sender()
+    {
+        string message = string.Empty;
+
+        while (message != "quitchat")
+        {
+            message = userInput("Enter message: ");
+            connection.InvokeAsync("SendMessage", message);
+        }
+    }
+
+    string userInput(string prompt)
+    {
+        Console.Write(prompt);
+        return Console.ReadLine();
+    }
+}
---
+++ Client/Program.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Client;
+
+internal class Program
+{
+
+
+    static async Task Main(string[] args)
+    {        
+        string ip = userInput("Enter IP: ");
+        string port = userInput("Enter port: ");
+        string endpoint = userInput("Enter endpoint: ");
+
+        var test = new Chat(ip, port, endpoint);
+
+
+
+        /*
+        string url = $"wss://{ip}:{port}/{endpoint}";
+
+        HubConnection connection = new HubConnectionBuilder().WithUrl("wss://localhost:5001/chathub").Build();
+
+
+        // Ny tråd
+
+        Thread t;
+
+        try
+        {
+            connection = new HubConnectionBuilder().WithUrl(url).Build();
+            connection.StartAsync().Wait();
+
+            t = new Thread(Listener);
+            t.Start();
+        }
+        catch (Exception)
+        {
+
+            Console.WriteLine("Connection failed");
+        }
+
+        var workT = new Thread(Worker);
+        workT.Start();
+
+
+        string message = "";
+
+        while (message != "quitchat")
+        {
+            message = userInput("Enter message: ");
+            connection.InvokeAsync("SendMessage", message);
+            
+        }
+
+        */
+        //connection.InvokeAsync("SendMessage", "Andra datorn");
+        Console.ReadKey();
+    }
+
+    
+
+    static string userInput(string prompt)
+    {
+        Console.Write(prompt);
+        return Console.ReadLine();
+    }
+}
+
+
+/*
+internal class Program
+{
+    static void Main(string[] args)
+    {      
+        var connection = new HubConnectionBuilder().WithUrl("wss://localhost:5001/chathub").Build();
+
+        connection.StartAsync().Wait();
+
+        connection.InvokeAsync("SendMessage", "Andra datorn");
+        Console.ReadKey();
+    }
+}
+*/
